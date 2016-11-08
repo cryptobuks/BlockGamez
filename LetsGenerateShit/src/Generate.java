495db266cb8b59d6768b2673d06eb06310aa53da
Benjamin Tiefenbach <tiefenb7@gmail.com>
2016-11-07 19:06:43 -0500

    Add SHA256 and RIPEMD160 Hashes

--- LetsGenerateShit/src/Generate.java
+++ LetsGenerateShit/src/Generate.java
@@ -2,15 +2,20 @@ import org.bouncycastle.asn1.sec.SECNamedCurves;
 import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.digests.RIPEMD160Digest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
 import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
 import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.math.ec.ECFieldElement;
+import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.util.encoders.Hex;
+
 import java.io.IOException;
 import java.security.*;
+import java.util.Base64;
 
 
 /**
@@ -26,6 +31,11 @@ public class Generate {
 
 
     }
+
+
+    // Tues - Brian flies in around 5pm
+    // Stay overnight on Tues.
+    // Wed - 1/2 day - go down to perrryburg then drive up
 
 
     public static String toHex(byte[] data) {
@@ -54,51 +64,51 @@ public class Generate {
         ECPublicKeyParameters publicKey = (ECPublicKeyParameters) keyPair.getPublic();
         byte[] privateKeyBytes = privateKey.getD().toByteArray();
 
-
         ECFieldElement getFirst = publicKey.getQ().getX();
         ECFieldElement getSecond = publicKey.getQ().getY();
         String finalPublic = "04" + getFirst.toString() + getSecond.toString();
+
 
         System.out.println("");
         // First print our generated private key and public key
         System.out.println("Private key: " + toHex(privateKeyBytes));
         System.out.println("Public key: " + finalPublic);
         System.out.println("");
-        String suo = sha256(finalPublic);
 
-        System.out.println("SHA-256 Hash on public key... ");
-        System.out.println(suo);
-        System.out.println("");
-        System.out.println("RIPMD160 on SHA256 Hash... ");
-        toRIPEMD160(suo);
-        System.out.println("");
+        ECPoint dd = ecp.getG().multiply(privateKey.getD());
+
+        byte[] publickey=new byte[65];
+        System.arraycopy(dd.getY().toBigInteger().toByteArray(), 0, publickey, 64-dd.getY().toBigInteger().toByteArray().length+1, dd.getY().toBigInteger().toByteArray().length);
+        System.arraycopy(dd.getX().toBigInteger().toByteArray(), 0, publickey, 32-dd.getX().toBigInteger().toByteArray().length+1, dd.getX().toBigInteger().toByteArray().length);
+        publickey[0]=4;
+
+        System.out.println("HELLO " + toHex(SHA256hash(publickey)));
+
+        byte[] newValue = SHA256hash(publickey);
+
+        System.out.println("HELLO160 " + toHex(RIPEMD160(newValue)));
+
+
+
+
     }
 
-    public static String sha256(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
+    private byte[] SHA256hash(byte[] enterKey){
+        SHA256Digest digester=new SHA256Digest();
+        byte[] retValue=new byte[digester.getDigestSize()];
+        digester.update(enterKey, 0, enterKey.length);
+        digester.doFinal(retValue, 0);
+        return retValue;
     }
 
-    public static void toRIPEMD160(String in) throws IOException {
-        byte[] r = in.getBytes("US-ASCII");
-        RIPEMD160Digest d = new RIPEMD160Digest();
-        d.update (r, 0, r.length);
-        byte[] o = new byte[d.getDigestSize()];
-        d.doFinal (o, 0);
-        Hex.encode (o, System.out);
-        System.out.println();
+
+    private byte[] RIPEMD160(byte[] enterKey){
+        RIPEMD160Digest digester = new RIPEMD160Digest();
+        byte[] retValue=new byte[digester.getDigestSize()];
+        digester.update(enterKey, 0, enterKey.length);
+        digester.doFinal(retValue, 0);
+       // byte[] version = new byte[]{0x00};
+       // return concateByteArray(version,retValue);
+        return retValue;
     }
 }
