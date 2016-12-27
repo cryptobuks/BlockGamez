c258cd729bee66122ec23debac9583e1ce9a2cef
tief <tiefenb7@gmail.com>
2016-12-27 11:47:42 -0500

    Added Next Steps, and Code Clean-up

--- BlockGamez-Solutions/SignedTransactions/src/SignedTransactions.java
+++ BlockGamez-Solutions/SignedTransactions/src/SignedTransactions.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 
@@ -21,7 +20,6 @@ public class SignedTransactions {
     public static void main(String args[]) throws IOException {
 
         SignedTransactions createTransaction = new SignedTransactions();
-
         /**
          * recipientAddress -> The private key we are sending a transaction to (In WIF Format)
          * senderAddress -> The WIF address that is sending the bitcoin
@@ -34,26 +32,17 @@ public class SignedTransactions {
         BigDecimal amount = new BigDecimal("0.01");
         BigDecimal transactionFee = new BigDecimal("0.0005");
         createTransaction.NewTransaction(recipientAddress, senderAddress, amount, transactionFee);
-
-
-
-
-
     }
 
     //Add the SATOSHI Constant (100,000,000 Satoshi = 1 BTC)
     public BigDecimal SATOSHI_PER_BITCOIN(){
 
-
         BigDecimal SATOSHI_PER_BITCOIN = new BigDecimal("100000000");
-
-
         return SATOSHI_PER_BITCOIN;
     }
 
 
     public String parseUrlInJson(String grabJsonValue, String sURL) throws IOException {
-
 
         // Connect to the URL using java's native library
         URL url = new URL(sURL);
@@ -61,22 +50,18 @@ public class SignedTransactions {
         request.connect();
 
         // Convert to a JSON object to print data
-        JsonParser jp = new JsonParser(); //from gson
-        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
-        String grabbedJsonValue = rootobj.get(grabJsonValue).getAsString(); //just grab the zipcode
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject rootobj = root.getAsJsonObject();
+        String grabbedJsonValue = rootobj.get(grabJsonValue).getAsString();
 
         return grabbedJsonValue;
-
     }
 
 
     public void NewTransaction(String recipientAddrress, String senderAddress, BigDecimal amount, BigDecimal transactionFee) throws IOException {
 
-
         System.out.println("About to send " + amount + " Bitcoin to address " + recipientAddrress + " from address " + senderAddress + " with a transaction fee of: " + transactionFee + "\n");
-
-
         /** URLs to blockchain's JSON Values **/
         String sFinalBalance = "https://blockchain.info/address/" + senderAddress + "?format=json"; //just a string
         String sUnspentOutputs = "https://blockchain.info/unspent?active=" + senderAddress + "&format=json";
@@ -103,10 +88,45 @@ public class SignedTransactions {
             //throw new IllegalArgumentException("Insufficient funds in Wallet");
             /** ###############################################################################################################################**/
         }
+        // Need to check for pending payments, will do so later when testing...
+        System.out.println(UnspentTransactions());
+        System.out.println(SignandVerify());
+    }
 
-        BigDecimal[] val = new BigDecimal[10];
-        Arrays.fill(val, BigDecimal.ZERO);
+    /** # Need to check wallet if there is a result of one or more incoming payments. **/
+    public String UnspentTransactions(){
 
+        String value = "Need to check for pending payments -> Skipping...";
+        return value;
+
+    }
+    /** Sign and verify the transaction **/
+    public String SignandVerify(){
+
+        /**
+        w2 = Bitcoin.decode_base58(@secret_wif)
+        w3 = w2[0..-9]
+        @secret = w3[2..-1]
+
+        @keypair = Bitcoin.open_key(@secret)
+        raise "Invalid keypair" unless @keypair.check_key
+
+        step_2 = (Digest::SHA2.new << [@keypair.public_key_hex].pack("H*")).to_s
+        step_3 = (Digest::RMD160.new << [step_2].pack("H*")).to_s
+        step_4 = "00" + step_3
+        step_5 = (Digest::SHA2.new << [step_4].pack("H*")).to_s
+        step_6 = (Digest::SHA2.new << [step_5].pack("H*")).to_s
+        step_7 = step_7 = step_6[0..7]
+        step_8 = step_4 + step_7
+        step_9 = Bitcoin.encode_base58(step_8)
+
+        raise "Public key does not match private key" if @sender != step_9
+
+        puts "Public key matches private key, so we can sign the transaction..."
+        **/
+
+        String value = "Need to sign and verify the transaction -> Skipping...";
+        return value;
     }
 
 }
