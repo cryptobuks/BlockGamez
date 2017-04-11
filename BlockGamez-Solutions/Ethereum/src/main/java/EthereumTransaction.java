5349b4340878d4d277a12c6310d9087cab47730e
tief <tiefenb7@gmail.com>
2017-04-11 19:53:37 -0400

    Entering Raw Transaction Code

--- BlockGamez-Solutions/Ethereum/src/main/java/EthereumTransaction.java
+++ BlockGamez-Solutions/Ethereum/src/main/java/EthereumTransaction.java
@@ -1,7 +1,10 @@
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.request.RawTransaction;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Convert;
 
+import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -10,11 +13,28 @@ import java.util.concurrent.ExecutionException;
 public class EthereumTransaction {
 
     public static void main(String args[]) throws ExecutionException, InterruptedException {
-        System.out.println("Hello World");
 
         Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
         Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
         String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+        System.out.println(clientVersion); //List Geth Client Version
+
+
+        /** Transfer Ether from One Party to Another **/
+
+
+        /** ############# Create Raw ################### **/
+        BigInteger nonceAmount = new BigInteger("1.0");
+        BigInteger gasPrice = new BigInteger("1.0");
+        BigInteger gasLimit = new BigInteger("1.0");
+        String sendToAddress = "Enter Address Here!";
+        BigInteger amount = Convert.toWei("1.0", Convert.Unit.ETHER).toBigInteger();
+
+        RawTransaction createRawTransaction = RawTransaction.createEtherTransaction(
+                nonceAmount,gasPrice,gasLimit,sendToAddress,amount
+        );
+
+        /** ############# Send ####################### **/
 
     }
 }
